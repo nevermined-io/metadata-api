d69140e6e126fc1e0bd8a5a7676eb4252be422b4
Rodolphe Marques <marques.rodolphe@gmail.com>
2021-08-05 14:32:48 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -75,6 +75,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/nevermined-io/metadata-api',
-    version='0.2.4',
+    version='0.3.0',
     zip_safe=False,
 )
