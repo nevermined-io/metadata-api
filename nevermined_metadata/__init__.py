ffb8d6a48b87466ec4b692822bf179d4667fd6e3
enrique <enrique@keyko.io>
2021-08-30 12:04:59 +0200

    Bump version: 0.3.1 → 0.3.2

--- nevermined_metadata/__init__.py
+++ nevermined_metadata/__init__.py
@@ -1,2 +1,2 @@
 __author__ = """Nevermined"""
-__version__ = '0.3.1'
+__version__ = '0.3.2'
--- setup.py
+++ setup.py
@@ -75,6 +75,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/nevermined-io/metadata-api',
-    version='0.3.1',
+    version='0.3.2',
     zip_safe=False,
 )
