5bf3cabae396f34b6d9266b6a4cdf8e91770cf4e
Rodolphe Marques <marques.rodolphe@gmail.com>
2021-09-07 13:56:58 +0200

    Switch back to gunicorn

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,5 +3,6 @@
 export CONFIG_PATH=/nevermined_metadata/config.ini
 export CONFIG_FILE=/nevermined_metadata/config.ini
 envsubst < /nevermined_metadata/config.ini.template > /nevermined_metadata/config.ini
-uwsgi --ini uwsgi.ini.template
+# uwsgi --ini uwsgi.ini.template
+gunicorn -b ${METADATA_URL#*://} -w ${METADATA_WORKERS} nevermined_metadata.run:app
 tail -f /dev/null
