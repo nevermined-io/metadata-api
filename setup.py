c51b971cc714506c90f315670d3aeaf0df6c11dc
ssallam <36411297+ssallam@users.noreply.github.com>
2022-06-07 12:38:12 +0200

    fix tests.

--- setup.py
+++ setup.py
@@ -13,8 +13,7 @@ with open('CHANGELOG.md') as history_file:
 
     install_requirements = [
     'coloredlogs==15.0.1',
-    'Flask==2.0.1',
-    'werkzeug==2.0.3',
+    'Flask==2.1.1',
     'Flask-Cors==3.0.10',
     'flask-swagger==0.2.14',
     'flask-swagger-ui==3.36.0',
