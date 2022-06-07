1b40e0d88856e4a1cf9a59bdc5554ddf913625b2
ssallam <36411297+ssallam@users.noreply.github.com>
2022-06-07 10:30:07 +0200

    allow setting logging level using envvar.

--- nevermined_metadata/log.py
+++ nevermined_metadata/log.py
@@ -6,13 +6,25 @@ import coloredlogs
 import yaml
 
 
-def setup_logging(default_path='logging.yaml', default_level=logging.INFO, env_key='LOG_CFG'):
+def setup_logging(default_path='logging.yaml', default_level=None, env_key='LOG_CFG'):
     """Logging Setup"""
     path = default_path
     value = os.getenv(env_key, None)
     if value:
         path = value
-    if os.path.exists(path):
+
+    if not default_level:
+        level_map = {
+            "INFO": logging.INFO,
+            "DEBUG": logging.DEBUG,
+            "WARNING": logging.WARNING,
+            "ERROR": logging.ERROR,
+        }
+        default_level = level_map.get(os.getenv("LOG_LEVEL", "INFO"), logging.INFO)
+
+    print(f'setup_logging: path={path}, level={default_level}, env-path={os.getenv(env_key, None)}')
+
+    if os.getenv("LOG_LEVEL", None) is None and os.path.exists(path):
         with open(path, 'rt') as f:
             try:
                 config = yaml.safe_load(f.read())
